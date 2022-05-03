2456e54ffe46b9a1d1b3e3fd753f33fba820b1b7
Parth Patel <parth.p@brilworks.com>
2022-05-03 17:52:27 +0530

    update main file

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ from routes.api import router as api_router
 from fastapi_sqlalchemy import DBSessionMiddleware , db
 from src.dependencies import env
 app = FastAPI()
-app.add_middleware(DBSessionMiddleware, db_url =  env.DATABASE_URL)
+
 origins = ["*"]
 
 app.add_middleware(
+DBSessionMiddleware,
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    db_url =  env.DATABASE_URL
 )
 
 app.include_router(api_router)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
-    print("running")
+ 
