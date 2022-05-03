4f47d7259fb393bd37512c4e185ba483b76a33b1
Parth Patel <parth.p@brilworks.com>
2022-05-03 17:44:28 +0530

    update

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ app.add_middleware(
 app.include_router(api_router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8005, log_level="info", reload=True)
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
     print("running")
