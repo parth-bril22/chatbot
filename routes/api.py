f60a7928f474f59728c24f92b26d865f11eb9254
Parth Patel <parth.p@brilworks.com>
2022-05-03 18:28:06 +0530

    clear logs

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.endpoints import login, node
+from src.endpoints import users, node
 router = APIRouter()
 router.include_router(users.router)
 router.include_router(node.router)
