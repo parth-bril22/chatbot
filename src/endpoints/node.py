0d2afbf250b7f7c5b1f124874942c1f2f7a80247
Parth Patel <parth.p@brilworks.com>
2022-05-30 12:56:45 +0530

    update create_node_with_conn api

--- src/endpoints/node.py
+++ src/endpoints/node.py
@@ -374,6 +374,9 @@ async def create_node_with_conn(my_node:NodeSchema , node_id:int, sub_node_id:st
         create_node_response, my_id = await create_node(node=my_node)
         if (create_node_response.status_code != 200):
             return create_node_response
+        sub_node = db.session.query(SubNode.id).filter_by(node_id=node_id).filter_by(id=sub_node_id).first()
+        if (sub_node == None):
+            return JSONResponse(status_code=200, content={"message": "No such subnode exists"})
         create_conn = ConnectionSchema(flow_id=my_node.flow_id, source_node_id=node_id,
                                   sub_node_id=sub_node_id,
                                   target_node_id=my_id)
