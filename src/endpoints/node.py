536757f56aeaa46902c1f2b43fe5137e2c967ed7
shaival-brilworks <106059972+shaival-brilworks@users.noreply.github.com>
2022-06-01 13:53:48 +0530

    update send api
    made previous_sub_node custom instead of direct passing of object

--- src/endpoints/node.py
+++ src/endpoints/node.py
@@ -581,17 +581,16 @@ async def send(flow_id : int, my_source_node:str, my_sub_node:str):
     try:
         nodes = []
         #get current data of current node
-        # previous_node = db.session.query(Node).filter_by(id = my_source_node).filter_by(flow_id=flow_id).first()
         previous_sub_node = db.session.query(SubNode).filter_by(node_id = my_source_node).filter_by(flow_id=flow_id).filter_by(id = my_sub_node).first()
+        previous_sub_node = {"flow_id":previous_sub_node.flow_id, "node_id":previous_sub_node.node_id, "type": previous_sub_node.type, "data":[previous_sub_node.data], "id":previous_sub_node.id }
         previous_sub_node = (encoders.jsonable_encoder(previous_sub_node))
-        # previous_node = (encoders.jsonable_encoder(previous_node))
 
 
         nn_row = db.session.query(Connections).filter_by(source_node_id = my_source_node).filter_by(sub_node_id = my_sub_node).filter_by(flow_id=flow_id).first()
         if(nn_row != None):
             is_end_node = False
         else:
-            return JSONResponse(status_code=200, content = {"next_node":[], "sub_node":[], "previous_sub_node": [previous_sub_node]})
+            return JSONResponse(status_code=200, content = {"next_node":[], "sub_node":[], "previous_sub_node": previous_sub_node})
 
         nn = "chat"#to enter loop
         #get the next node from Connections table
@@ -641,7 +640,7 @@ async def send(flow_id : int, my_source_node:str, my_sub_node:str):
 
         db.session.commit()
         db.session.close()
-        return {"next_node":nodes, "sub_node": sub_nodes,"is_end__node" : is_end_node, "previous_sub_node": [previous_sub_node]}
+        return {"next_node":nodes, "sub_node": sub_nodes,"is_end__node" : is_end_node, "previous_sub_node": previous_sub_node}
     except Exception as e:
         print("Error at send: ", e)
         return JSONResponse(status_code=404, content={"message": "Send Chat data : Not Found"})
