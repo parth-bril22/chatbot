6b44c8595b0411bbad64a6e39da065b94cbd9eff
Parth Patel <parth.p@brilworks.com>
2022-06-22 16:14:30 +0530

    changes

--- src/endpoints/flow.py
+++ src/endpoints/flow.py
@@ -211,18 +211,18 @@ async def save_draft(flow_id:int,token = Depends(auth_handler.auth_wrapper)):
         print(e, "at:", datetime.now())
         return JSONResponse(status_code=400, content={"message":"please check the input"})
  
-# @router.post('/preview')
-# async def tokenize_preview(flow_id,token = Depends(auth_handler.auth_wrapper)):
-#     try:
-#         flow_id =  db.session.query(Flow.id).filter_by(publish_token = my_token).first()[0]
+@router.post('/preview')
+async def tokenize_preview(my_token:str,token = Depends(auth_handler.auth_wrapper)):
+    try:
+        flow_id =  db.session.query(Flow.id).filter_by(publish_token = my_token).first()[0]
 
-#         if(my_token in db.session.query(Flow.publish_token).filter_by(publish_token = my_token).first()[0]):
-#             return await preview(flow_id, token = Depends(auth_handler.auth_wrapper))
-#         else:
-#             return JSONResponse(status_code = 404, content={"message":"Cannot open preview. Token not identified"})
-#     except Exception as e:
-#         print("Error: in  my_token/preview", e)
-#         return JSONResponse(status_code = 404, content={"message":"Cannot open preview"})
+        if(my_token in db.session.query(Flow.publish_token).filter_by(publish_token = my_token).first()[0]):
+            return await preview(flow_id, token = Depends(auth_handler.auth_wrapper))
+        else:
+            return JSONResponse(status_code = 404, content={"message":"Cannot open preview. Token not identified"})
+    except Exception as e:
+        print("Error: in  my_token/preview", e)
+        return JSONResponse(status_code = 404, content={"message":"Cannot open preview"})
     
 @router.post('/publish')
 async def publish(flow_id: int,diagram : Dict,token = Depends(auth_handler.auth_wrapper)):
