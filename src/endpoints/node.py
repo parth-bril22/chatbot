20dc8acccc78b7259058e10106ed1ba658b91a3d
Parth Patel <parth.p@brilworks.com>
2022-06-29 10:31:39 +0530

    fix the jumpflow issue

--- src/endpoints/node.py
+++ src/endpoints/node.py
@@ -226,9 +226,9 @@ async def update_node(node_id:str,my_node:NodeSchema,token = Depends(auth_handle
         if(node_check.status_code != 200):
             return node_check
         
-        relevant_fields = db.session.query(SubNode.data).filter_by(node_id=node_id).first()
-        relevant_fields = (relevant_fields)[0].keys()
-        db.session.commit()
+        # relevant_fields = db.session.query(SubNode.data).filter_by(node_id=node_id).first()
+        # relevant_fields = (relevant_fields)[0].keys()
+        # db.session.commit()
             
         #update node data
         db.session.query(Node).filter(Node.id == node_id).filter_by(flow_id=my_node.flow_id).update({'data' : node_data, 'type' : my_node.type, 'position':my_node.position})
@@ -313,7 +313,9 @@ async def update_sub_node(my_sub_node:SubNodeSchema,sub_node_id:str = Body(...),
         sub_nodes = db.session.query(SubNode).filter_by(flow_id=my_sub_node.flow_id).filter_by(node_id = my_sub_node.node_id).all()
         node_data = []
         for sub_node in sub_nodes:
-            node_data.append(sub_node.data)    
+            node_data.append(sub_node.data)  
+        db.session.query(Node).filter_by(flow_id=my_sub_node.flow_id).filter_by(id = my_sub_node.node_id).update({'data' : my_data})
+        db.session.commit()  
         db.session.query(Flow).filter_by(id = my_sub_node.flow_id).update({'updated_at' : datetime.now(timezone.utc)})
         db.session.commit()
         db.session.close()
