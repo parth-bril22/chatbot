11636173baa4fe98abc888a9de7e2b66569a57d6
Parth Patel <parth.p@brilworks.com>
2022-07-07 14:46:35 +0530

    server issue

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ app.add_middleware(
 )
 
 app.include_router(api_router)
-templates = Jinja2Templates(directory="demo")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
