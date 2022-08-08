35fcb411a23b221f57821d1a8f1f4009f882beea
Parth Patel <parth.p@brilworks.com>
2022-08-08 11:43:57 +0530

    code refactor

--- src/endpoints/flow.py
+++ src/endpoints/flow.py
@@ -565,30 +565,12 @@ async def upload_to_s3_from_user(file:UploadFile,node_id:int,flow_id:int):
         
         if(db.session.query(Node).filter_by(id = node_id).filter_by(flow_id=flow_id).first() == None):
             return JSONResponse(status_code=404, content={"errorMessage":"Node not found"})
-    
-        if file.content_type == 'image/png':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'image/png'})
-        elif file.content_type == 'image/jpeg':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'image/jpeg'})
-        elif file.content_type == 'image/jpg':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'image/jpg'})
-        elif file.content_type == 'image/gif':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'image/gif'})  
-        elif file.content_type in 'video/mp4':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'video/mp4'})  
-        elif file.content_type == 'text/html':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'text/html'})
-        elif file.content_type == 'text/plain':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'text/plain'})
-        elif file.content_type == 'application/msword':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'application/msword'})
-        elif file.content_type == 'application/pdf':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'application/pdf'})
-        elif file.content_type == 'audio/mpeg':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'audio/mpeg'})
-        elif file.content_type == 'text/csv':
-            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':'text/csv'})
 
+        CONTENT_TYPES = ['image/png','image/jpeg','image/jpg','image/gif','video/mp4','text/html','image/svg+xml','text/plain','application/msword','application/pdf',
+        'audio/mpeg','text/csv','application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+
+        if file.content_type in CONTENT_TYPES:
+            bucket.upload_fileobj(file.file,'userfiles/'+str(flow_id)+'/'+str(node_id)+'/'+(file.filename),ExtraArgs={'ContentType':file.content_type})
 
         s3_file_url = f"https://{BUCKET_NAME}.s3.ap-south-1.amazonaws.com/userfiles/{flow_id}/{node_id}/{file.filename}"
         return JSONResponse(status_code=200,content={"message":"Successfully Uploaded","url":s3_file_url})
