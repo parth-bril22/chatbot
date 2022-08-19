129be8a9a6edc49d66f1c5873246c3fc9d5c2d04
Parth Patel <parth.p@brilworks.com>
2022-08-19 16:05:35 +0530

    add  slack integration in conversation

--- src/endpoints/flow.py
+++ src/endpoints/flow.py
@@ -424,8 +424,8 @@ async def save_chat_history(chats:ChatSchema,token = Depends(auth_handler.auth_w
     """
     Save the chat history of every user
     """
+    global client
     try:
-        global client
         valid_user = await check_user_token(chats.flow_id,token)
         if (valid_user.status_code != 200):
             return valid_user
