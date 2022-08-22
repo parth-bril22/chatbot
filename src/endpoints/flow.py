fa5759b249d893c0bcbe37b01b1cae0d1f0187b6
Parth Patel <parth.p@brilworks.com>
2022-08-22 18:25:01 +0530

    Revert "fix bug"
    This reverts commit cabc10489cee401c8ff9132a95845c0d3d468901.

--- src/endpoints/flow.py
+++ src/endpoints/flow.py
@@ -420,6 +420,7 @@ async def get_flow_detail(flow_id:int,token = Depends(auth_handler.auth_wrapper)
 
 async def post_message(slack_id,message):
     slack_db = db.session.query(Slack).filter_by(id=slack_id).first()
+    print(slack_db.bot_token)
     client = WebClient(token=slack_db.bot_token)
 
     try:
@@ -466,6 +467,7 @@ async def save_chat_history(chats:ChatSchema,token = Depends(auth_handler.auth_w
             # db.session.query(Flow).filter_by(id = chats.flow_id).update({"finished":finish})
             for ch in chats.chat:
                 if ch['type']=='slack':
+                    print(ch['data']['text'])
                     await post_message(int(ch['data']['slack_id']),ch['data']['text'])
                 else:
                     pass
