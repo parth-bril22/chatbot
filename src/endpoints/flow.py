b842cebd2cab9336db8c835e9a53da227656fb21
Parth Patel <parth.p@brilworks.com>
2022-08-23 14:25:22 +0530

    fix bug

--- src/endpoints/flow.py
+++ src/endpoints/flow.py
@@ -231,8 +231,8 @@ async def save_draft(flow_id:int):
     try:
         diagram = await get_diagram(flow_id)
         for node in get_diagram['nodes']:
-            if node.type=='slack':
-                if node.data['slack_id'] == None:
+            if node['type']=='slack':
+                if node['data']['slack_id'] == None:
                     return JSONResponse(status_code=400, content={"errorMessage": "No slack channel selected"})
         db.session.query(Flow).filter_by(id = flow_id).update({'diagram' : diagram})
         db.session.commit()
