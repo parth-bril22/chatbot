ca26ca74336f2285e5c219b639f4061685df0f0d
Parth Patel <parth.p@brilworks.com>
2022-09-05 13:59:22 +0530

    not allowed same name var

--- src/endpoints/customfields.py
+++ src/endpoints/customfields.py
@@ -28,9 +28,9 @@ async def create_global_variable(schema:GlobalVariableSchema):
         if schema.type in types:
 
             # check not same name variable
-            # var_names = [i[0] for i in db.session.query(Variable.name).filter_by(flow_id=schema.flowId).all()]
-            # if var_names in var_names:
-            #     return JSONResponse(status_code=404,content={"errorMessage":"Name already exists"})
+            var_names = [i[0] for i in db.session.query(Variable.name).filter_by(flow_id=schema.flowId).all()]
+            if var_names in var_names:
+                return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,content={"errorMessage":"The variable name "  +{schema.name}+ "is not allowed"})
             # create the variable
             var = Variable(name = schema.name,type = schema.type,flow_id=schema.flowId)
             db.session.add(var)
@@ -39,7 +39,7 @@ async def create_global_variable(schema:GlobalVariableSchema):
 
             return JSONResponse(status_code=status.HTTP_201_CREATED,content={"message":"Created successfully"})
         else:
-            return JSONResponse(status_code=404,content={"errorMessage":"Type is not correct"})
+            return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE,content={"errorMessage":"Type is not correct"})
     except Exception as e:
         print(e,"at check user. Time:", datetime.now())
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,content={"errorMessage":"Can't create a variable"})
