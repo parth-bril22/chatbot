4c58e1137765578ec6411248bb1c783d0716cd79
Parth Patel <parth.p@brilworks.com>
2022-09-08 13:56:23 +0530

    remove node_id

--- src/schemas/customfieldSchema.py
+++ src/schemas/customfieldSchema.py
@@ -5,8 +5,6 @@ class GlobalVariableSchema(BaseModel):
     name:str
     type:str
     userId:int
-    nodeId:int
-
     class Config:
         orm_mode = True
         underscore_attrs_are_private = True
